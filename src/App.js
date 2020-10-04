fc99f5636547293f257843f13dabf6ec06f76531
zx012 <rrai9999999@gmail.com>
2020-10-04 14:14:36 +0530

    Added Navbar and Button Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
